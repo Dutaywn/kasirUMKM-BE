6519259685eb0321e7d0421b0030f1d762873f7e
ridhoduta <ridhodutay@gmail.com>
2026-03-23 22:22:20 +0700

    update cors

--- src/index.ts
+++ src/index.ts
@@ -40,7 +40,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
-app.options("/*", cors());
+app.options(/.*/, cors());
 
 app.get("/", (req, res) => {
   res.json({ message: "UMKM Kasir API is running! 🚀" });
